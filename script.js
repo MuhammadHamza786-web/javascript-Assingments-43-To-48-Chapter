2f2421d68edae4d71ad5aad83e6c6b38aa09e6cd
MuhammadHamza786-web <hamzasiraj.470@gmail.com>
2026-02-12 09:43:17 +0500

    add Assingments

--- script.js
+++ script.js
@@ -2,3 +2,17 @@
 // 2. Display some Mobile images in browser. On click on an
 // image Show the message in alert to user.
 
+function mobile1_Image(){
+    alert("Samsung Smartphone — Stunning display, powerful performance aur stylish design ke sath ek complete smart experience.");
+}
+function mobile2_Image(){
+    alert("Vivo Smartphone — Stylish look, sharp camera aur smooth speed jo daily use ke liye perfect hai.");
+}
+function mobile3_Image()
+{
+    alert(" OnePlus Smartphone — Fast performance, smooth display, powerful camera aur premium design ke sath ek flagship experience.");
+ }
+
+// 3. Display 10 student records in table and each row should contain a delete
+// button. If you click on a button to delete a record, entire row should be
+// deleted. 
