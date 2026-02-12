a10728615dcc890216bed535923e06e56f715600
MuhammadHamza786-web <hamzasiraj.470@gmail.com>
2026-02-13 00:55:45 +0500

    Add code

--- script.js
+++ script.js
@@ -16,12 +16,56 @@ function mobile3_Image()
 // 3. Display 10 student records in table and each row should contain a delete
 // button. If you click on a button to delete a record, entire row should be
 // deleted. 
-
+let students=[];
 function getData(){
     let userName  =document.getElementById("userName").value;
-    let userRollNumber=document.getElementById("rollNumber").value;
-    document.getElementById("tableData").innerHTML=`${userName} ${userRollNumber}`
+    let userRollNumber=Number(document.getElementById("rollNumber").value);
+    if(userName ===""){
+        alert("Please Enter Your Name")
+
+    }
+    //  if(userRollNumber ===""){
+    //     alert("Please Enter Your Roll Number")
+
+    // }
+    let studentobj={
+        name:userName,
+        RollNumber:userRollNumber
+    }
+    students.push(studentobj)
+    printdata()
 }
 
-function printdata(){
-}
+        function deleteStudent(index){
+        
+            students.splice(index, 1)
+
+            printdata()
+        }
+
+
+
+        function printdata(){
+            let rows = ""
+            for(let i=0; i < students.length; i++){
+                rows += `
+                    <tr>
+                        <td>${i}</td>
+                        <td>${students[i].name}</td>
+                        <td>${students[i].RollNumber}</td>
+                        <td>
+                            <button onClick="deleteStudent(${i})">Delete</button>
+                        </td>
+                    </tr>
+                `
+            }
+
+            document.getElementById("tableData").innerHTML = rows
+        }
+
+//         4. Display an image in browser. Change the picture on mouseover and set the
+// first picture on mouseout.
+
+// 5. Show a counter in browser. Counter should increase on click on increase
+// button and decrease on click on decrease button. And show updated counter
+// value in browser.
